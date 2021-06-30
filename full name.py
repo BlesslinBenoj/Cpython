e6bcb57d8771a4c31b664ea686eb860e31191f11
LogicTenacity <logictenacityftw@gmail.com>
2021-06-30 00:52:14 -0700

    first commit

---
+++ full name.py
@@ -0,0 +1,20 @@
+"""
+
+first_name = "logic"
+last_name = "tenacity"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+"""
+"""
+first_name = "logic"
+last_name = "tenacity"
+full_name = f"{first_name} {last_name}"
+print(f"Hello,{full_name.title()}!")
+
+"""
+first_name = "logic"
+last_name = "tenacity"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
