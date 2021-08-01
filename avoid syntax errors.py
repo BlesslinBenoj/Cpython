8bc8fd32287348e562bdfc47a4e944d322d367d4
LogicTenacity <85760673+LogicTenacity@users.noreply.github.com>
2021-08-01 10:39:27 -0700

    Delete avoid syntax errors.py

--- avoid syntax errors.py
+++
@@ -1,8 +0,0 @@
-message = "One of python's Strength is its diverse community"
-print(message)
-
-
-#dont use '' ,if the para has quotation already
-
-message='One of python's Strength is its diverse community'
-print(message)
